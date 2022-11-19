73de54b841f5bd7788992ad3008c7ea5df5fb3b9
Ravi <raviyadav4224@gmail.com>
2022-11-20 01:07:37 +0530

    added frontend url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
