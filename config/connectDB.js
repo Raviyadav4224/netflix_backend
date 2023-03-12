127b3bd62b1c08742aa4f98102f194d421ec4256
Ravi <raviyadav4224@gmail.com>
2023-03-12 21:31:58 +0530

    updated strictQuerry of mongoDb

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,6 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 const connectDB = async () => {
+  mongoose.set("strictQuery", false);
   const { connection } = await mongoose.connect(process.env.MONGODB_URL);
   console.log(`Mongodb is running at ${connection.host}`);
 };
